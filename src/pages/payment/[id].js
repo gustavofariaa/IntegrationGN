4b2336d27118c590039721e42df98bfd967c7663
Gustavo <gufariaa@gmail.com>
2021-03-05 09:37:02 -0300

    Initial commit

--- src/pages/payment/[id].js
+++ src/pages/payment/[id].js
@@ -7,20 +7,20 @@ import api from '../../utils/api';
 export default function Payment() {
   const { query: { id } } = useRouter();
 
-  const [payment, setPayment] = useState(undefined)
+  const [payment, setPayment] = useState(undefined);
 
   useEffect(() => {
     if (!id) return;
     (async () => {
       try {
         const { data } = await api.get(`/api/payment/${id}`);
-        setPayment(data)
-      } catch(error) { 
+        setPayment(data);
+      } catch (error) {
         console.error(error);
         setPayment(null);
       }
-    })()
-  }, [id])
+    })();
+  }, [id]);
 
   return <PaymentPage payment={payment} />;
 }
